6cc123203344c4bc8f861529e4fd981c93470a5c
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-02-08 12:41:10 +0530

    Initial files

---
+++ src/main/kotlin/oop/Classes.kt
@@ -0,0 +1,29 @@
+package oop
+
+fun main() {
+    val myCar = Car()
+    val yourCar = Car()
+
+    myCar.model = "BMW M4 competition"
+    myCar.topSpeed = 320
+    myCar.start()
+    myCar.drive(69)
+    println("Top speed of ${myCar.model} is ${myCar.topSpeed}")
+
+    yourCar.model = "Ford Mustang"
+    yourCar.start()
+    yourCar.drive(67)
+}
+
+class Car {
+    var model: String? = null
+    var topSpeed = 100
+
+    fun start() {
+        println("Starting the $model")
+    }
+
+    fun drive(speed: Int) {
+        println("Driving at a speed of $speed")
+    }
+}
