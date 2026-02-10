937a2dfa18466222047a2d05389bae03ca720b7b
Raghunadan-79 <raghunandansharma1905@gmail.com>
2026-02-10 21:11:03 +0530

    Practice of with keyword

--- src/main/kotlin/section5standardfunctions/PracticeWith.kt
+++ src/main/kotlin/section5standardfunctions/PracticeWith.kt
@@ -1,2 +1,43 @@
 package section5standardfunctions
 
+fun main() {
+    // Question 1
+    with(Person()) {
+        firstName = "Raghunandan"
+        lastName = "Sharma"
+        age = 21
+        printInformation()
+    }
+
+    // Question 2
+    with(Store()) {
+        shoes = 100
+        shirts = 50
+        jackets = 10
+        printInventory()
+    }
+}
+
+class Person {
+    var firstName: String = ""
+    var lastName: String = ""
+    var age:Int = 1
+
+    fun printInformation() {
+        println("Firstname is: ${this.firstName}\n" +
+                "Lastname is: ${this.lastName}\n" +
+                "Age is: ${this.age}")
+    }
+}
+
+class Store {
+    var shoes: Int = 0
+    var shirts: Int = 0
+    var jackets: Int = 0
+
+    fun printInventory() {
+        println("This store has ${this.shoes} shoes\n" +
+                "This store has ${this.shirts} shirts\n" +
+                "This store has ${this.jackets} jackets")
+    }
+}
