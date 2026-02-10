b9557d64f0b6fac3fa6184113347bc70adf8c609
Raghunadan-79 <raghunandansharma1905@gmail.com>
2026-02-10 21:03:48 +0530

    With standard function

---
+++ src/main/kotlin/section5standardfunctions/With.kt
@@ -0,0 +1,16 @@
+package section5standardfunctions
+
+fun main() {
+    with(Car()) {
+        speed = 80
+        drive()
+        println("Car is driving")
+    }
+}
+
+class Car {
+    var speed = 50
+    fun drive() {
+        println("Driving at $speed")
+    }
+}
