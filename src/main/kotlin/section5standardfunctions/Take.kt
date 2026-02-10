0c1c11220f482e2daee169554c430b1ce4396a84
Raghunadan-79 <raghunandansharma1905@gmail.com>
2026-02-10 21:54:37 +0530

    takeIf and takeUnless standard functions

--- src/main/kotlin/section5standardfunctions/Take.kt
+++ src/main/kotlin/section5standardfunctions/Take.kt
@@ -1,2 +1,12 @@
 package section5standardfunctions
 
+import kotlin.random.Random
+
+fun main() {
+    val number = Random.nextInt(100)
+    val evenOrNull = number.takeIf { it % 2 == 0 }
+    println(evenOrNull)
+
+    val oddOrNull = number.takeUnless { it % 2 == 0 }
+    println(oddOrNull)
+}
